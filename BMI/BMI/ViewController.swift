99690e1aeef86ddf992b84b10f609ac51cac2506
FAlmutairi <feras.saud10@gmail.com>
2022-01-07 12:03:05 +0300

    Add a new images and link it with code and add BMI formula

--- BMI/BMI/ViewController.swift
+++ BMI/BMI/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var hightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSilderChanged(_ sender: UISlider) {
+        
+        let hight = String(format: "%.2f", sender.value)
+        hightLabel.text = "\(hight) M"
+        
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        
+        let weight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weight) KG"
+        
+    }
+    
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        let heightCalualate = heightSlider.value
+        let weightCalualate = weightSlider.value
+        
+        let BMI = weightCalualate / (heightCalualate * heightCalualate)
+        print(BMI)
+        
+    }
+    
 }
 
